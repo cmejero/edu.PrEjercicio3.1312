b57dd051f63c8b27ece8221ebb4c331dd884e9fb
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2023-12-18 14:04:36 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace edu.PR.Ejercicio3._1312
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[,] numero = new int[10, 10];
+
+
+            int valor = 100;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    numero[i, j] = valor;
+                    valor--;
+                }
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(numero[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
